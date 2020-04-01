c928978e6c5304a8d83a83f386c781381a359724
yo-squirrel <squirrel_tech@qq.com>
2020-04-01 19:01:21 +0800

    添加 OAuth 示例代码

--- config/default.js
+++ config/default.js
@@ -1,1 +1,5 @@
-module.exports = {}
+module.exports = {
+  clientId: '创建 GitHub OAuth 应用生成的客户端 ID',
+  clientSecret: '创建 GitHub OAuth 应用生成的客户端密钥',
+  redirectUrl: 'http://127.0.0.1:3000/redirect',
+}
--- router.js
+++ router.js
@@ -1,8 +1,58 @@
 const Router = require('@koa/router')
+const config = require('config')
+const axios = require('axios')
 const router = new Router()
 
 router.get('/', async (ctx) => {
-  await ctx.render('index')
+  await ctx.render('index', {
+    clientId: config.clientId,
+    redirectUrl: config.redirectUrl,
+  }, true)
+})
+
+router.get('/redirect', async (ctx) => {
+  const code = ctx.query.code // Code 为 GitHub 拼接在重定向链接后的参数
+
+  if (!code) {
+    ctx.throw(400, '回调 URL 无 code 字段')
+    return
+  }
+
+  // GitHub OAuth 要求参数
+  const param = {
+    client_id: config.clientId,
+    client_secret: config.clientSecret,
+    code,
+  }
+
+  // GitHub 交换访问令牌默认返回字符串，通过设置 accept 来控制结果返回 json 字符串
+  const opt = {
+    headers: {
+      accept: 'application/json',
+    },
+  }
+
+  // 使用 code 交换访问令牌
+  const { data } = await axios.post('https://github.com/login/oauth/access_token', param, opt)
+  const accessToken = data.access_token
+
+  if (!accessToken) {
+    ctx.throw(500, '交换访问令牌失败')
+    return
+  }
+
+  const { data: user } = await axios.get('https://api.github.com/user', {
+    headers: {
+      Authorization: `token ${accessToken}`,
+    },
+  })
+
+  if (!user) {
+    ctx.throw(500, '用户数据获取失败')
+    return
+  }
+
+  ctx.body = user
 })
 
 module.exports = router
